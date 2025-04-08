43f9e26ba582751eb92def5476090da0be11e210
Dendy Faist <dendy@fai.st>
2025-04-08 17:12:10 +0200

    feat: Allocate it all on the heap beforehand

--- pic3.c
+++ pic3.c
@@ -1,4 +1,6 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 FILE *testFile;
@@ -6,8 +8,11 @@ FILE *testFile;
 int gridWidth, gridHeight, sqSize, nFrames;
 long headerEnd;
 
-int FetchData() {
-  int data = 0;
+// Pointer to the array of frames that will be initialized in the heap
+uint8_t *frames = NULL;
+
+uint8_t FetchData() {
+  uint8_t data = 0;
   int buffer;
 
   while ((buffer = fgetc(testFile)) != EOF) {
@@ -29,19 +34,25 @@ void printColorTerminal(int r, int g, int b) {
 }
 
 void loop() {
+  int frameIdx = 0;
+
   for (int currentFrame = 0; currentFrame < nFrames; currentFrame++) {
     int imgIndex = 0;
 
     for (int n = gridWidth / 2; n > 0; n--) {
       for (int i = gridHeight; i > 0; i--) {
-        printColorTerminal(FetchData(), FetchData(), FetchData());
+        int r = frameIdx;
+        printColorTerminal(frames[r], frames[r + 1], frames[r + 2]);
         imgIndex++;
+        frameIdx += 3;
       }
       puts("");
 
       for (int i = gridHeight; i > 0; i--) {
-        printColorTerminal(FetchData(), FetchData(), FetchData());
+        int r = frameIdx;
+        printColorTerminal(frames[r], frames[r + 1], frames[r + 2]);
         imgIndex++;
+        frameIdx += 3;
       }
       puts("");
     }
@@ -49,8 +60,6 @@ void loop() {
     clear();
     usleep(200000);
   }
-
-  fseek(testFile, headerEnd, SEEK_SET);
 }
 
 int main() {
@@ -72,12 +81,26 @@ int main() {
   nFrames = FetchData();
   printf("nFrames:   %2d\n", nFrames);
 
-  headerEnd = ftell(testFile);
-  printf("headerEnd: %2ld\n", headerEnd);
+  // Allocates in heap an array of n bytes. (sizeof(char) is 1 (byte))
+  // So it doesn't really mean much in here but it's good practice to put it
+  unsigned long length = gridWidth * gridHeight * nFrames * 3 * sizeof(char);
+  frames = malloc(length);
+
+  // Read every byte into the buffer
+  for (int i = 0; i < length; i++) {
+    frames[i] = FetchData();
+  }
+
+  // We don't need to have it open anymore, let's close it
+  fclose(testFile);
 
   while (1) {
     loop();
   }
+
+  // Every time you stop using something that has been malloc'd you gotta free
+  // it. Obviously we're never reaching here cuz it's intifinite, but yknow.
+  free(frames);
 
   return 0;
 }
